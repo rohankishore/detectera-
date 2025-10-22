3aa109e8572e3df76e1a69f5f4a2d6fe14b1d3a3
Rohan Kishore <rohankishore746@outlook.com>
2025-10-22 22:01:03 +0530

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 # If MODEL_BASE_URL and MODEL_FILES are provided, download the models into MODEL_DEST_DIR
 if [ -n "$MODEL_BASE_URL" ] && [ -n "$MODEL_FILES" ]; then
   echo "Fetching models from $MODEL_BASE_URL"
-  python download_models.py
+  python ../download_models.py
 else
   echo "MODEL_BASE_URL or MODEL_FILES not set; skipping model download"
 fi
